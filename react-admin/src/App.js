6c4cb5a4d87194ef236048fa7f7d1142d7ed6e43
Siddharth (Antariksh) <siddharth.asterisk@gmail.com>
2023-02-16 21:12:14 +0530

    charts completed

--- react-admin/src/App.js
+++ react-admin/src/App.js
@@ -13,13 +13,13 @@ import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
 
 import Calendar from "./scenes/calendar";
+import FAQ from "./scenes/faq";
 
-// import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
-// import Pie from "./scenes/pie";
+import Bar from "./scenes/bar";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
 
-// import FAQ from "./scenes/FAQ";
-// import Geography from "./scenes/geography";
+import Geography from "./scenes/geography";
 
 
 
@@ -55,16 +55,13 @@ function App() {
               <Route path="/form" element={<Form />} />
 
               <Route path="/calendar" element={<Calendar/>} /> 
-{/*               
-              <Route path="/line" element={<Line/>} />
-              <Route path="/bar" element={<Bar/>} />
-              <Route path="/pie" element={<Pie/>} />
               <Route path="/FAQ" element={<FAQ/>} />
+
+              <Route path="/bar" element={<Bar/>} />
+              <Route path="/line" element={<Line/>} />
+              <Route path="/pie" element={<Pie/>} />
+              
               <Route path="/geography" element={<Geography/>} />
-*/}
-
-
-
             </Routes> 
           </main>
 
